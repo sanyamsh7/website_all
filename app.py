5d794bd294b3b5044aa81d999c34a22468cc37e7
sanyamsh7 <sanyam19sharma@gmail.com>
2020-09-14 19:08:32 +0530

    login 14 sept 2020-07:10p.m

---
+++ app.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 14 17:29:50 2020
+
+@author: Dell
+"""
+
+from flask import Flask, render_template, url_for, flash, redirect
+
+
+#app initialisation
+app = Flask(__name__)
+
+
+@app.route("/")
+@app.route("/login")
+def home():
+    return render_template('login.html')
+
+
+@app.route("/register")
+def register():
+    return render_template('registration.html')
+
+
+
+##Main loop
+if __name__ == '__main__':
+    print("Application is running")
+    app.run()
